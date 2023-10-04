830f76b226a7fba55b72fdd9e69ff661e48a99ec
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-04 14:46:27 +0100

    REGEX that catches occurrence of optional b in hbtn

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# REGEX that accepts one or no occurrence of b in this string hbtn
+# REGEX that accepts ONE or NO occurrence of b in this string hbtn
 puts ARGV[0].scan(/hb?tn/).join
