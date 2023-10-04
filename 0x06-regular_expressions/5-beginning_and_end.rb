c640cc3e1912968b72ce583942336fba2ca7b3a9
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-04 15:15:26 +0100

    allowing only 1 of any character to be in between h and n

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# The regular expression must be exactly matching a string
+#+ that starts with h ends with n and can have any single character in between
+puts ARGV[0].scan(/h[A-Za-z0-9]n/).join
