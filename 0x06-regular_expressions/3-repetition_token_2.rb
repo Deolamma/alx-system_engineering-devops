0fb1762f302e06a64188ef73f04f29be418f2cff
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-04 15:03:00 +0100

    Regex for 0 or more occurrence of the word t in hbtn

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-# Multiple repetition of the t char in hbtn
-puts ARGV[0].scan(/hb[t]+n/).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Zero or more of the t char in hbtn
+puts ARGV[0].scan(/hb[t]*n/).join
