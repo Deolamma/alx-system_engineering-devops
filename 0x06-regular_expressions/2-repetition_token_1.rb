c80f7bc31d846585f0f728069eef1ee43190bd7e
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-04 14:56:01 +0100

    alllows for multiple occurrences of the t char in hbtn

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-# REGEX that accepts ONE or NO occurrence of b in this string hbtn
-puts ARGV[0].scan(/hb?tn/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Multiple repetition of the t char in hbtn
+puts ARGV[0].scan(/hb[t]+n/).join
